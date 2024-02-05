9b25f5121641a62520e3258785a6ed668a407dd6
Chris Coomber <ccoomber@whiterosemaths.com>
2024-02-05 15:13:01 +0000

    Update with workaround and more shapes

--- App.js
+++ App.js
@@ -4,26 +4,64 @@ import {
   Gesture,
 } from 'react-native-gesture-handler';
 import { View, Text } from 'react-native';
-import { Svg, Path } from 'react-native-svg';
+import { Svg, Path, Circle } from 'react-native-svg';
 import { useState, useCallback } from 'react';
 
 export default function App() {
-  const path = 'M 0 0 L 0 50 L 50 50 L 50 0 Z';
+  const path = 'M 0 0 L 0 100 L 100 100 L 100 0 Z';
 
-  const [fill, setFill] = useState('red');
-  const switchColor = useCallback(() => setFill(old => old === 'red' ? 'green' : 'red'), [setFill]);
+  const [squareFill, setSquareFill] = useState('red');
+  const switchSquareColor = useCallback(
+    () => setSquareFill((old) => (old === 'red' ? 'green' : 'red')),
+    [setSquareFill]
+  );
 
-  const tapGesture = Gesture.Tap()
-    .runOnJS(true)
-    .onEnd(switchColor);
+  const [circleFill, setCircleFill] = useState('blue');
+  const switchCircleColor = useCallback(
+    () => setCircleFill((old) => (old === 'blue' ? 'brown' : 'blue')),
+    [setCircleFill]
+  );
+
+  const tapGestureSquare = Gesture.Tap().runOnJS(true).onEnd(switchSquareColor);
+  const tapGestureCircle = Gesture.Tap().runOnJS(true).onEnd(switchCircleColor);
 
   return (
     <GestureHandlerRootView style={{ flex: 1, paddingTop: 200 }}>
-      <Text>Click on the square to change color</Text>
+      <Text>(Original issue: broken on iOS) Click on the shapes to change their color</Text>
       <View style={{ padding: 10, borderWidth: 1, alignSelf: 'flex-start' }}>
-        <Svg width={100} height={100}>
-          <GestureDetector gesture={tapGesture}>
-            <Path d={path} fill={fill} />
+        <Svg width={200} height={200}>
+          <GestureDetector gesture={tapGestureSquare}>
+            <Path d={path} fill={squareFill} />
+          </GestureDetector>
+          <GestureDetector gesture={tapGestureCircle}>
+            <Circle
+              r={200}
+              cx={210}
+              cy={210}
+              fill={circleFill}
+            />
+            {/* Or equivalently in a path - same behaviour */}
+            {/* <Path d='M10,210a200,200 0 1,0 400,0a200,200 0 1,0 -400,0'  fill={circleFill} onResponderMove={() => {}} /> */}
+          </GestureDetector>
+        </Svg>
+      </View>
+
+      <Text>(With workaround) Click on the shapes to change their color</Text>
+      <View style={{ padding: 10, borderWidth: 1, alignSelf: 'flex-start' }}>
+        <Svg width={200} height={200}>
+          <GestureDetector gesture={tapGestureSquare}>
+            <Path d={path} fill={squareFill} onResponderMove={() => {}} />
+          </GestureDetector>
+          <GestureDetector gesture={tapGestureCircle}>
+            <Circle
+              r={200}
+              cx={210}
+              cy={210}
+              fill={circleFill}
+              onResponderMove={() => {}}
+            />
+            {/* Or equivalently in a path - same behaviour */}
+            {/* <Path d='M10,210a200,200 0 1,0 400,0a200,200 0 1,0 -400,0'  fill={circleFill} onResponderMove={() => {}} /> */}
           </GestureDetector>
         </Svg>
       </View>
